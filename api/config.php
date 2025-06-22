edd264261c448ac5414c31d74f6ec8866aea3391
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-06-22 22:22:31 +0200

    connexion a la base de donnée

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+//connexion a la base de données
+$host = 'localhost';
+$dbname = 'social_connect';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
