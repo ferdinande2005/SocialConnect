e03a4a2d06d3b50c74ae360477e5e19e75f37684
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-06-25 00:48:44 +0200

    ajout de phpmailer pour l'envoi de message

--- api/config/phpmailer.php
+++ api/config/phpmailer.php
@@ -1,13 +1,13 @@
 <?php
-// Configuration PHPMailer
-$phpmailer_config = [
-    'host' => 'smtp.gmail.com',
-    'port' => 587,
-    'smtp_auth' => true,
-    'username' => 'votre-email@gmail.com',
-    'password' => 'votre-mot-de-passe-app',
-    'smtp_secure' => 'tls',
-    'from_email' => 'no-reply@socialconnect.com',
-    'from_name' => 'SocialConnect'
-];
+    // Configuration PHPMailer
+    $phpmailer_config = [
+        'host' => 'smtp.gmail.com',
+        'port' => 587,
+        'smtp_auth' => true,
+        'username' => 'socialconnect48@gmail.com',
+        'password' => 'pqxb keiq urpj lmzx',
+        'smtp_secure' => 'tls',
+        'from_email' => 'socialconnect48@gmail.com',
+        'from_name' => 'SocialConnect'
+    ];
 ?>
