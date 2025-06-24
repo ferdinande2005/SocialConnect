4c81ff3fc7c1518b6e6b8ec55fb4227d8bdff938
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-06-24 21:25:04 +0200

    ajout du fichier de configuration de php mailer

---
+++ api/config/phpmailer.php
@@ -0,0 +1,13 @@
+<?php
+// Configuration PHPMailer
+$phpmailer_config = [
+    'host' => 'smtp.gmail.com',
+    'port' => 587,
+    'smtp_auth' => true,
+    'username' => 'votre-email@gmail.com',
+    'password' => 'votre-mot-de-passe-app',
+    'smtp_secure' => 'tls',
+    'from_email' => 'no-reply@socialconnect.com',
+    'from_name' => 'SocialConnect'
+];
+?>
